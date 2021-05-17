3a65f85552d0b00042bb3a9397dd55a7f549afbc
tlee <ksltho@gmail.com>
2021-05-17 16:39:43 -0400

    Complete p03_hash_set.rb

--- skeleton 2/lib/p03_hash_set.rb
+++ skeleton 2/lib/p03_hash_set.rb
@@ -7,18 +7,36 @@ class HashSet
   end
 
   def insert(key)
+    resize! if @count + 1 > num_buckets
+    hash = key.hash
+    
+    if !self.include?(key)
+      self[hash] << key
+      @count += 1
+    end
+
   end
 
   def include?(key)
+    hash = key.hash
+    self[hash].include?(key)
   end
 
   def remove(key)
+    hash = key.hash
+    
+    if self.include?(key)
+      self[hash].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    # p (num % num_buckets)
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -26,5 +44,11 @@ class HashSet
   end
 
   def resize!
+    temp = @store.flatten
+    @store = Array.new(num_buckets*2) {Array.new}
+    
+    temp.each do |ele|
+      self[ele] << ele
+    end
   end
 end
